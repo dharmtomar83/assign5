ed3e30036d3e88654ecd7c3ffe4b526ef234fb79
dharm <dharm@LAPTOP-OROP1HV7>
2023-08-28 14:23:21 +0530

    Add project files.

---
+++ assign5/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+public partial class assign5
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+using System;
+
+public partial class assign5
+{
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        assign5 person = new assign5
+        {
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        person.PrintFullName();
+    }
+}
